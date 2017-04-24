289ff882c839bd39759cd75748527d9f641534cf
danniel113616 <113616435@qq.com>
2017-04-24 17:46:04 +0800

    单单www文件第一次提交
    单单www文件第一次提交

---
+++ config.js
@@ -0,0 +1,7 @@
+//快递首页home 广告下的图片地址
+var EXPRESS_IMG_URL = 'http://10.118.212.21:8090/cdnsit/wap/images/timepro.png';
+//广告内容
+var EXPRESS_IMG_URL_CONTENT = 'http://ucmp.sf-express.com/weiwap/newVersion/moduleHomePage/sf-aging.html';
+
+//广州海关链接地址
+var GZHG_LINK_CONTENT = 'http://www.baidu.com';
